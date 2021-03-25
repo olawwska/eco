f3fd8c00b8569710a7f747c756aeb850d54ea8af
olawwska <olwojciech@gmail.com>
2021-03-25 20:45:43 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Container } from "./Components/Container";
+import Container from "./Components/Container";
 
 import { GoogleApiWrapper } from "google-maps-react";
 
--- src/Components/Container.js
+++ src/Components/Container.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
 import { Map, Marker, InfoWindow } from "google-maps-react";
 
@@ -6,122 +6,125 @@ import firebase from "../firebase";
 
 import Header from "./Header";
 
-export class Container extends Component {
-    state = {
+const Container = (props) => {
+    const [mapVeganState, setMapVeganState] = useState({
         Vegan: [],
+    });
+
+    const [mapGlutenfreeState, setMapGlutenfreeState] = useState({
         GlutenFree: [],
-        passedSelectedOption1: {},
-        passedSelectedOption2: {},
+    });
+
+    const [headerSelectState, setHeaderSelectState] = useState({
+        passedSelectedOption1: "",
+        passedSelectedOption2: "",
+    });
+
+    const [infoWindowState, setInfoWindowState] = useState({
         showingInfoWindow: false,
-        activeMarker: {},
-        selectedPlace: {},
+        activeMarker: null,
+        selectedPlace: null,
+    });
+
+    const onMarkerClick = (marker) => {
+        setInfoWindowState({
+            showingInfoWindow: true,
+            activeMarker: marker,
+            selectedPlace: props,
+        });
     };
 
-    componentDidMount() {
+    const onMapClick = () => {
+        if (setInfoWindowState.showingInfoWindow) {
+            setInfoWindowState({
+                showingInfoWindow: false,
+                activeMarker: null,
+                selectedPlace: null,
+            });
+        }
+    };
+
+    useEffect(() => {
         const itemsRef = firebase.database().ref("Vegan");
         itemsRef.on("value", (snapshot) => {
             let vegans = snapshot.val();
-            this.setState({
+            setMapVeganState({
                 Vegan: vegans,
             });
         });
         const itemsRef2 = firebase.database().ref("Glutenfree");
         itemsRef2.on("value", (snapshot) => {
             let glutenfrees = snapshot.val();
-            this.setState({
+            setMapGlutenfreeState({
                 GlutenFree: glutenfrees,
             });
         });
-    }
+    }, []);
 
-    handleClickButton = (paramFromChild1, paramFromChild2) => {
-        this.setState({
+    const handleClickButton = (paramFromChild1, paramFromChild2) => {
+        setHeaderSelectState({
             passedSelectedOption1: paramFromChild1,
             passedSelectedOption2: paramFromChild2,
         });
     };
 
-    handleMarkerCreate = (places) => {
+    const handleMarkerCreate = (places) => {
         return Object.values(places)
             .filter((place) => {
                 return (
-                    this.state.passedSelectedOption1.value === place.locality &&
-                    this.state.passedSelectedOption2.value === place.type
+                    headerSelectState.passedSelectedOption1.value === place.locality &&
+                    headerSelectState.passedSelectedOption2.value === place.type
                 );
             })
             .map((place) => {
                 return (
                     <Marker
-                        onClick={this.onMarkerClick}
                         key={place.name}
-                        restaurant={place}
                         position={{ lat: place.latitude, lng: place.longitude }}
+                        onClick={() => {
+                            onMarkerClick(place);
+                        }}
                     />
                 );
             });
     };
 
-    onMarkerClick = (props, marker) => {
-        this.setState({
-            selectedPlace: props,
-            activeMarker: marker,
-            showingInfoWindow: true,
-        });
-        console.log(this.state.selectedPlace.restaurant);
-    };
-
-    onMapClick = () => {
-        if (this.state.showingInfoWindow) {
-            this.setState({
-                activeMarker: null,
-                showingInfoWindow: false,
-            });
-        }
-    };
-
-    render() {
-        const style = {
-            width: "100vw",
-            height: "100vh",
-        };
-
-        if (!this.props.loaded) {
-            return <div>Loading...</div>;
-        }
-
-        return (
-            <div style={style}>
-                <Header clickMethod={this.handleClickButton}></Header>
-                <Map
-                    style={{
-                        width: "70%",
-                        height: "70%",
-                        margin: "0 auto",
-                        border: " 10px solid hsl(0, 0%, 80%)",
-                    }}
-                    google={this.props.google}
-                    zoom={14}
-                    initialCenter={{ lat: 52.229676, lng: 21.012229 }}
-                    onClick={this.onMapClick}
+    return (
+        <div style={{ width: "100vw", height: "100vh" }}>
+            <Header clickMethod={handleClickButton}></Header>
+            <Map
+                style={{
+                    width: "70%",
+                    height: "70%",
+                    margin: "0 auto",
+                    border: " 10px solid hsl(0, 0%, 80%)",
+                    zIndex: "-1",
+                }}
+                google={props.google}
+                zoom={14}
+                initialCenter={{ lat: 52.229676, lng: 21.012229 }}
+                onClick={onMapClick}
+                onChildClick
+            >
+                {handleMarkerCreate(mapVeganState.Vegan)}
+                {handleMarkerCreate(mapGlutenfreeState.GlutenFree)}
+                <InfoWindow
+                    marker={infoWindowState.activeMarker}
+                    visible={infoWindowState.showingInfoWindow}
                 >
-                    {this.handleMarkerCreate(this.state.Vegan)}
-                    {this.handleMarkerCreate(this.state.GlutenFree)}
-                    <InfoWindow
-                        marker={this.state.activeMarker}
-                        visible={this.state.showingInfoWindow}
-                    >
-                        {this.state.showingInfoWindow === true ? (
-                            <div>
-                                <h1>{this.state.selectedPlace.restaurant.name}</h1>
-                                <h3>{this.state.selectedPlace.restaurant.type}</h3>
-                                <h3>{this.state.selectedPlace.restaurant.locality}</h3>
-                                <h3>{this.state.selectedPlace.restaurant.address}</h3>
-                                <h3>{this.state.selectedPlace.restaurant.url}</h3>
-                            </div>
-                        ) : null}
-                    </InfoWindow>
-                </Map>
-            </div>
-        );
-    }
-}
+                    {/* {infoWindowState.showingInfoWindow === true ? ( */}
+                    <div>
+                        {/* <h1>{infoWindowState.selectedPlace.restaurant.name}</h1>
+                            <h3>{infoWindowState.selectedPlace.restaurant.type}</h3>
+                            <h3>{infoWindowState.selectedPlace.restaurant.locality}</h3>
+                            <h3>{infoWindowState.selectedPlace.restaurant.address}</h3>
+                            <h3>{infoWindowState.selectedPlace.restaurant.url}</h3> */}
+                    </div>
+                    {/* ) : null} */}
+                </InfoWindow>
+            </Map>
+        </div>
+    );
+};
+
+export default Container;
